ea62bc1bb58926688c4c9928f68d52594452ee25
ewaseda <ewaseda@ewa.com>
2023-06-09 15:03:39 +0300

    chapter 08 added

--- src/task/c.java
+++ src/task/c.java
@@ -2,12 +2,8 @@ package task;
 
 public class c {
     public static void main(String[] args) {
-        String str="Ahmet";
-       String ksrt=str.toLowerCase();
 
-       String bstr=ksrt.replace("a","");
-       String estr=bstr.replace("e","a");
-        System.out.println( estr.replace("", "e"));
+
 
     }
 }
